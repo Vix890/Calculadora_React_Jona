a9bcc0ed7b4e9ca33f75a5a0c89dc31c7f77fa42
Vix <you@example.com>
2023-12-16 14:27:53 +0100

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,214 +1,79 @@
-import React from "react";
+import "./App.css";
+import logogato from "../src/imagenes/logo.gif";
+import Boton from "./componentes/Boton.jsx";
+import Display from "./componentes/Display";
+import BotonClear from "./componentes/BotonClear";
 import { useState } from "react";
+//libreria matematica (npm install mathjs)
+import { evaluate } from "mathjs";
+import Borrar from "./componentes/borrar";
 
 function App() {
-  const [display, setDisplay] = useState("0");
-  const [operations, setOperations] = useState("");
-  const [evalued, setEvalued] = useState(false);
-
-  const handleNumber = (event) => {
-    const number = event.target.textContent;
-
-    if (operations.length >= 15) {
-      return;
-    }
-
-    // introduce el número
-    setDisplay(display === "0" || evalued === true ? number : display + number);
-    setOperations(operations + number);
-    setEvalued(false);
+  //estado de la memoria de un componente  (Hook)
+  //Declaracion de una variable de estado que hemos llamdo input
+  const [input, setInput] = useState("");
+  //definimos la funcion
+  const agregarInput = (val) => {
+    setInput(input + val);
   };
 
-  // manejar la entrada de .
-  const handlePoint = (event) => {
-    const point = event.target.textContent;
-
-    const lastNumber = operations.split(" ").pop();
-
-    if (operations.length >= 15) {
-      return;
+  const borrarInput = () => {
+    try {
+      let aux = input - input[input.length - 1];
+      //remover ultimo caracter ingresado
+      aux = input.substring(0, input.length - 1);
+      setInput(aux);
+    } catch {
+      setInput(input);
     }
-
-    // dos puntos
-    if (lastNumber.includes(".")) {
-      return;
-    }
-    // normal
-    setOperations(operations + point);
-    setDisplay(display === "0" ? point : display + point);
-    setEvalued(false);
   };
 
-  // manejar la entrada de operadores
-  const handleOperator = (event) => {
-    // saca el operador introducido
-    const operator = event.target.textContent;
-    // caracter anterior
-    const lastChat = operations.charAt(operations.length - 2);
-    // comprueba si el caracter anterior es un operador
-    const lastChatIsOperator = ["+", "*", "/", "-"].includes(lastChat);
-
-
-    if (operations.length >= 15) {
-      return;
+  //funtion que evalua el calculo a resolver(recordar libreria mathjs)
+  const calcularResultado = () => {
+    try {
+      setInput(evaluate(input));
+    } catch {
+      setInput("SytaxisError");
     }
-    
-    // manejo de errores
-    if (display === "") {
-      setOperations("SyntaxError");
-      setDisplay("SyntaxError");
-      setEvalued(false);
-      return;
-    }
-    
-    // condicion para el + -
-    if (lastChatIsOperator && operator !== "-") {
-      setDisplay(`${display.substring(0, display.length - 2)}${operator} `);
-      setOperations(
-        `${operations.substring(0, operations.length - 2)}${operator} `
-        );
-        setEvalued(false);
-        return;
-    }
-      
-      // nada antes del operador
-    if (operations === "") {
-      setDisplay(
-      display === "0"
-      ? "0" + " " + operator + " "
-      : display + " " + operator + " "
-      );
-      setOperations(display + " " + operator + " ");
-      setEvalued(false);
-      return;
-    }
-      
-    // normal
-    setDisplay(display === "0" ? operator : display + " " + operator + " ");
-    setOperations(operations + " " + operator + " ");
-    setEvalued(false);
   };
-
-  //   limpiar la pantalla
-  const handleClear = () => {
-    setDisplay("0");
-    setOperations("");
-    setEvalued(false);
-  };
-
-  // hacer las operaciones
-  const handleEval = () => {
-    if (operations) {
-      const result = eval(operations);
-
-      setDisplay(result.toString());
-      setOperations("");
-      setEvalued(true);
-
-      return;
-    }
-
-    setDisplay("0");
-    setOperations("");
-    setEvalued(true);
-  };
-
   return (
-    <article className="calculator">
-      <div className="formulaScreen">{operations}</div>
-      <div className="outputScreen" id="display">
-        {display}
+    <div className="App">
+      <div className="main-logo">
+        <img src={logogato} className="logo" alt="logo gato" />
       </div>
-      <React.Fragment>
-        <button
-          key="clear"
-          className="jumbo"
-          id="clear"
-          value="AC"
-          onClick={handleClear}
-        >
-          AC
-        </button>
-        <button
-          key="divide"
-          className="operator"
-          id="divide"
-          value="/"
-          onClick={handleOperator}
-        >
-          /
-        </button>
-        <button
-          key="multiply"
-          className="operator"
-          id="multiply"
-          value="*"
-          onClick={handleOperator}
-        >
-          *
-        </button>
-        <button key="seven" id="seven" value="7" onClick={handleNumber}>
-          7
-        </button>
-        <button key="eight" id="eight" value="8" onClick={handleNumber}>
-          8
-        </button>
-        <button key="nine" id="nine" value="9" onClick={handleNumber}>
-          9
-        </button>
-        <button
-          key="subtract"
-          className="operator"
-          id="subtract"
-          value="-"
-          onClick={handleOperator}
-        >
-          -
-        </button>
-        <button key="four" id="four" value="4" onClick={handleNumber}>
-          4
-        </button>
-        <button key="five" id="five" value="5" onClick={handleNumber}>
-          5
-        </button>
-        <button key="six" id="six" value="6" onClick={handleNumber}>
-          6
-        </button>
-        <button
-          key="add"
-          className="operator"
-          id="add"
-          value="+"
-          onClick={handleOperator}
-        >
-          +
-        </button>
-        <button key="one" id="one" value="1" onClick={handleNumber}>
-          1
-        </button>
-        <button key="two" id="two" value="2" onClick={handleNumber}>
-          2
-        </button>
-        <button key="three" id="three" value="3" onClick={handleNumber}>
-          3
-        </button>
-        <button
-          key="zero"
-          className="jumbo"
-          id="zero"
-          value="0"
-          onClick={handleNumber}
-        >
-          0
-        </button>
-        <button key="decimal" id="decimal" value="." onClick={handlePoint}>
-          .
-        </button>
-        <button key="equals" id="equals" value="=" onClick={handleEval}>
-          =
-        </button>
-      </React.Fragment>
-    </article>
+      <div className="container-calculadora">
+        <Display input={input} />
+        <div className="fila">
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={calcularResultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>^</Boton>
+        </div>
+        <div className="fila">
+          {/* creamos una funcion anonima */}
+          <BotonClear manejarClear={() => setInput("")}>Clear</BotonClear>
+          <Borrar quitarInput={borrarInput}>Borrar</Borrar>
+        </div>
+      </div>
+    </div>
   );
 }
 
